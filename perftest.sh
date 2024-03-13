e55e00f7894ecbaa13d5574894ee884797c3dd43
s-kontrol <servicios.mruiz@gmail.com>
2024-03-13 00:09:14 -0300

    Added error handling if we dont have apache benchmark

--- perftest.sh
+++ perftest.sh
@@ -6,7 +6,7 @@ else
     ./clear_logs.sh
     echo "IP address of fastapi-proxy-1 is $IP"
     echo "Running performance tests..."
-    ab -n 40000 -c 2 -k -r http://$IP:8080/counter | tee -a log/benchmark.log
+    ab -n 40000 -c 2 -k -r http://$IP:8080/counter | tee -a log/benchmark.log || echo "Apache Benchmark is not installed. Please install apache2-utils."
     echo "Results are in log/benchmark.log"
     exit 0
 fi
