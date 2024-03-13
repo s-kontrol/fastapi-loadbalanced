8b2d2898d513678bdefcf2272ed0d6957af5a89f
s-kontrol <servicios.mruiz@gmail.com>
2024-03-13 00:08:48 -0300

    Added error handling if we dont have apache benchmark

--- run_and_perftest.sh
+++ run_and_perftest.sh
@@ -7,4 +7,4 @@ docker-compose up -d --build --remove-orphans
 echo "Waiting 5 seconds to start performance tests.."
 sleep 5
 echo "Running performance tests..."
-./perftest.sh
+./perftest.sh || echo "Apache Benchmark is not installed. Please install apache2-utils."
